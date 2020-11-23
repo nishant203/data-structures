77dbd1d94b7e3bd181441dcf30aa699ab784872f
nishant203 <nishant203@gmail.com>
2020-11-23 16:08:34 +0530

    Added hashtable

---
+++ hash-tables/hashtable.ts
@@ -0,0 +1,36 @@
+export class HashTable {
+  private data: any[];
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  private _hash(key): number {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  public set(key, value): any[] {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  public get(key): any {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
+import { HashTable } from './hash-tables/hashtable';
 // Import stylesheets
 import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+/// Hashtable
+const hashtable  = new HashTable(50);
+hashtable.set('Nishant', '25');
+console.log(hashtable.get('Nishant'))
+///End 
